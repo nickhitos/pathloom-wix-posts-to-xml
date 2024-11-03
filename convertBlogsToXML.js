5ebe047a57f4f23d60ef1bb25f06f8d755d69d04
jordan-ngo <ngojord@gmail.com>
2024-11-03 10:24:57 -0800

    refactored code and implemented pagination

--- convertBlogsToXML.js
+++ convertBlogsToXML.js
@@ -2,7 +2,6 @@ const { Builder, By, until } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const { create } = require("xmlbuilder2");
 const fs = require("fs");
-
 const BLOG_URL = "https://www.pathloom.com/all-blogs";
 
 // Set Chrome options for headless mode
@@ -10,68 +9,75 @@ const options = new chrome.Options();
 options.addArguments("--headless"); // Run in headless mode
 options.addArguments("--no-sandbox");
 options.addArguments("--disable-dev-shm-usage");
+const service = new chrome.ServiceBuilder('/usr/bin/chromedriver'); // Path to ChromeDriver
 
 // Initialize the Selenium WebDriver (using Chrome in this case)
 // const driver = new Builder().forBrowser('chrome').build();
 const driver = new Builder()
 	.forBrowser("chrome")
 	.setChromeOptions(options)
+	.setChromeService(service) // Add this line to set the ChromeDriver service path
 	.build();
 
 const fetchAllBlogs = async () => {
 	try {
-		// Open the page with all blogs
-		await driver.get(BLOG_URL);
-
-		// Wait until blog links are loaded (adjust the class name accordingly)
-		await driver.wait(
-			until.elementsLocated(By.css(".gallery-item-container a")),
-			10000
-		);
-
-		// Get all blog links
-		const blogLinks = await driver.findElements(
-			By.css(".gallery-item-container a")
-		);
-		const blogCount = blogLinks.length;
-
+		let page = 1;
 		let blogData = [];
 
-		// Iterate through each blog link and visit the blog page
-		for (let i = 0; i < blogCount; i++) {
-			// Re-fetch blog links after navigation to avoid StaleElementReferenceError
-			const currentBlogLinks = await driver.findElements(
+		while (true) {
+			// Navigate to the current page
+			await driver.get(`${BLOG_URL}/page/${page}`);
+			console.log(`Scraping page ${page}...`);
+
+            try {
+                // Wait until blog links are loaded
+                await driver.wait(
+                    until.elementsLocated(By.css(".gallery-item-container a")),
+                    10000
+                );
+            } catch {
+                // Break if no blog links are found (end of pages)
+                break;
+            }
+
+			// Get all blog links on the current page
+			const blogElements = await driver.findElements(
 				By.css(".gallery-item-container a")
 			);
-			const link = await currentBlogLinks[i].getAttribute("href");
 
-			// Visit each blog page
-			await driver.get(link);
+            const links = [];
+            for (let href of blogElements) {
+                links.push(await href.getAttribute("href"));
+            }
 
-			// Wait for the blog content to load
-			await driver.wait(
-				until.elementLocated(By.css(".blog-post-page-font")),
-				10000
-			);
+			// Iterate through each blog link for the current page
+			for (let link of links) {
+				console.log(`Blog link: ${link}`);
 
-			const title = await driver
-				.findElement(By.css(".post-title"))
-				.getText();
-			const date = await driver
-				.findElement(By.css(".post-metadata__date"))
-				.getText();
-			const content = await driver
-				.findElement(By.css(".blog-post-page-font"))
-				.getText();
+				// Visit each blog page
+				await driver.get(link);
 
-			blogData.push({ title, link, date, content });
+				// Wait for the blog content to load
+				await driver.wait(
+					until.elementLocated(By.css(".blog-post-page-font")),
+					10000
+				);
 
-			// Go back to the blog listing page
-			await driver.navigate().back();
-			await driver.wait(
-				until.elementsLocated(By.css(".gallery-item-container a")),
-				10000
-			);
+				const title = await driver
+					.findElement(By.css(".post-title"))
+					.getText();
+				const date = await driver
+					.findElement(By.css(".post-metadata__date"))
+					.getText();
+				const content = await driver
+					.findElement(By.css(".blog-post-page-font"))
+					.getText();
+
+				blogData.push({ title, link, date, content });
+			}
+
+			// Move to the next page
+			page++;
 		}
 
 		return blogData;
